4764bd48dbf4e30b14ced83a464687003a996387
unknown <angeles_96_13@hotmail.com>
2024-09-07 09:51:21 -0400

    clase 2 python

---
+++ funciones.py
@@ -0,0 +1,26 @@
+#def NombreDeFuncion(Parametros):
+
+nombre="Maria"
+numero1=9
+numero2=4
+
+
+def Saludo():
+    print("Hola mucho gusto "+ nombre)
+
+def  Sumar():
+    num1=10
+    num2=20
+    #print(num1 + num2)
+    suma= num1 + num2
+    return suma
+
+def Resta(numero1,numero2):
+
+    resta= numero1-numero2
+    return resta 
+
+print(Resta(numero1,numero2)+10)
+
+
+#Saludo()   
---
+++ while.py
@@ -0,0 +1,10 @@
+#While (condicion):
+    # Lo que esta dentro del while
+    # Se ejecuta hasta que la condicion sea falsa
+
+edad=10
+
+while edad < 10:
+    print("Hola")
+
+    
